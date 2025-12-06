104e049cf73a08691de276bf663102c43eff5304
SeraphielSpark <taiwoemmanuel435@gmail.com>
2025-12-05 16:46:36 -0800

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/reactn/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   plugins: [react()],
 })
